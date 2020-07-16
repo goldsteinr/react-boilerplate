997160ef310ba295fb8ee4ce2a511081addaa401
Ricardo Goldstein <ricardo.goldstein@zee-dog.com>
2020-07-15 21:13:37 -0300

    build boilerplate

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const withPWA = require('next-pwa') // eslint-disable-line
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
